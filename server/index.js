944dbf82527f6ce3c5229583bdabc28540a049f9
Shayne Meyer <shayne.meyer@gmail.com>
2017-01-20 07:49:58 -0800

    add mongodb model for user

--- server/index.js
+++ server/index.js
@@ -3,12 +3,13 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-
 const app = express();
+const router = require('./router');
 
 // app setup
 app.use(morgan('combined')); // logging requests
 app.use(bodyParser.json({ type: '*/*' }));
+router(app);
 
 // server setup
 const port = process.env.PORT || 3090;
