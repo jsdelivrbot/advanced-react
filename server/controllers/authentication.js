92d223ec3fc830c31c9415add9e77ac2c217d5a7
Shayne Meyer <shayne.meyer@gmail.com>
2017-01-20 10:28:01 -0800

    finished auth server

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,4 +1,21 @@
 const User = require('../models/user');
+const jwt = require('jwt-simple');
+const config = require('../config');
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+
+  return jwt.encode({
+    sub: user.id,
+    iat: timestamp
+  }, config.secret);
+}
+
+exports.signin = function (req, res, next) {
+  // User email and password auth'd
+  // We just need to give them a token
+  res.send({ token: tokenForUser(req.user)});
+}
 
 exports.signup = function (req, res, next) {
   const email = req.body.email;
@@ -27,7 +44,7 @@ exports.signup = function (req, res, next) {
       if(err) { return next(err); }
 
       // Respond to request indicating the user was created
-      res.json({success: true});
+      res.json({token: tokenForUser(user)});
 
     }); // save to database
   });
--- server/models/user.js
+++ server/models/user.js
@@ -13,17 +13,29 @@ const userSchema = new Schema({
 userSchema.pre('save', function (next) {
   const user = this;
 
+  // generate a salt then run callback
   bcrypt.genSalt(10, function (err, salt) {
     if(err) { return next(err); }
 
+    // hash our password using the salt
     bcrypt.hash(user.password, salt, null, function (err, hash) {
       if(err) { return next(err); }
 
+      // overwrite plain text password with encrypted password
       user.password = hash;
+      // save the model
       next();
     });
   });
 });
+
+userSchema.methods.comparePassword = function (candidatePassword, callback) {
+  bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+    if(err) { return callback(err); }
+
+    callback(null, isMatch);
+  });
+}
 
 // Create the model class
 const ModelClass = mongoose.model('user', userSchema);
