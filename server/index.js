49c8f66c0f13ba474f51ece280837dc5ffa29852
Shayne Meyer <shayne.meyer@gmail.com>
2017-01-20 07:25:19 -0800

    add dependencies

---
+++ server/index.js
@@ -0,0 +1,17 @@
+// app starting point
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const app = express();
+
+// app setup
+app.use(morgan('combined')); // logging requests
+app.use(bodyParser.json({ type: '*/*' }));
+
+// server setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on ' + port);
