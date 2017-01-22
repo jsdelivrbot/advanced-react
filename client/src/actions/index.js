c91159aaaa9a40a6c364f0b893edd861a2138d0c
Shayne <shayne.meyer@gmail.com>
2017-01-21 17:38:30 -0800

    updated package.json

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,18 +1,23 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
 
 const ROOT_URL = 'http://localhost:3090';
 
-export function signInUser({ email, password}) {
+export function signinUser({ email, password}) {
   return function (dispatch) {
     // Submit email/password to server
-    axios.post(`${ROOT_URL}/signin`, { email, password });
+    axios.post(`${ROOT_URL}/signin`, { email, password })
+      .then(response => {
+        // If request is good...
+        // - Update state to indicate user is authenticated
+        // - Save the JWT token
+        // - redirect to the route '/feature'
+        browserHistory.push('/feature');
+      })
+      .catch(() => {
+        // If request is bad...
+        // - Show an error to the user
 
-    // If request is good...
-    // - Update state to indicate user is authenticated
-    // - Save the JWT token
-    // - redirect to the route '/feature'
-
-    // If request is bad...
-    // - Show an error to the user
+      });
   }
 }
--- client/src/components/auth/signin.js
+++ client/src/components/auth/signin.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
 import * as actions from '../../actions';
+import { connect } from 'react-redux';
+import compose  from 'compose';
+
 
 class Signin extends Component {
   handleFormSubmit({ email, password }){
-    console.log(email, password);
-    this.props.signInUser({ email, password });
+    this.props.signinUser({ email, password });
   }
 
   render() {
     const { handleSubmit, fields: { email, password }} = this.props;
 
     return (
-      <form onSubmit={this.handleFormSubmit.bind(this)}>
+      <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
         <fieldset className="form-group">
           <label>Email:</label>
           <input className="form-control" {...email} />
@@ -27,7 +29,10 @@ class Signin extends Component {
   }
 }
 
-export default reduxForm({
-  form: 'signin',
-  fields: ['email', 'password']
-}, null, actions)(Signin);
+export default compose(
+  connect(null, actions),
+  reduxForm({
+    form: 'signin',
+    fields: ['email', 'password']
+  })
+)(Signin);
